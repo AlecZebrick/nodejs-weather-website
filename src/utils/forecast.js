df08f8107076bf7aa958059c220669c054bd432b
Alexander Zebrick <azebrick1@yahoo.com>
2020-11-12 11:18:11 +0900

    Added humidity

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,5 +1,7 @@
 const request = require('request')
 
+
+
 const forecast = (latitude, longitude, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=a243610b0865d603985b9e40826f5916&query=' + latitude + ',' + longitude + '&units=f'
 
@@ -9,7 +11,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions + ' It is currently ' + body.current.temperature + ' degress out. There is a ' + body.current.precip+ '% chance of rain.')
+            callback(undefined, body.current.weather_descriptions + ' It is currently ' + body.current.temperature + ' degress out. There is a ' + body.current.precip+ '% chance of rain. The humidity is currently ' + body.current.humidity + '%.')
         }
     })
 }
